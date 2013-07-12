65bd8a0ccb2c9d908753d75ee90f9e73ad05ce83
moznion <moznion@gmail.com>
2013-07-12 16:56:48 +0900

    Implement parser().
    [CAUTION] Not tested yet....

--- perl/lib/Parser.pm
+++ perl/lib/Parser.pm
@@ -1,6 +1,8 @@
 package Parser;
 use strict;
 use warnings;
+use autodie;
+use Log;
 
 sub new {
     my ($class, %args) = @_;
@@ -8,6 +10,22 @@ sub new {
 }
 
 sub parse {
+    my $self = shift;
+
+    my @log_object;
+    {
+        open my $fh, '<', 'log.ltsv';
+        while (my $line = <$fh>) {
+            my %log;
+            for my $log_element (split(/\t/, $line)) {
+                my ($key, $value) = $log_element =~ /^([^:]*):(.*)/;
+                next if $value eq '-';
+                $log{$key} = $value;
+            }
+            push @log_object, Log->new(%log);
+        }
+    }
+    return \@log_object;
 }
 
 1;
