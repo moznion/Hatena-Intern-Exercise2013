a4fb0aef519545f8db201f759b8975fabe5ed7cc
moznion <moznion@gmail.com>
2013-07-15 02:18:23 +0900

    Implement function to show the log table on web view.

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,11 @@
-// 課題 JS-3 の実装をここに記述してください。
+var showLogTableOn = function(dom, logObjectList) {
+    return function() {
+        createLogTable(dom, logObjectList);
+    }
+};
+
+var submitButton      = document.getElementById("submit-button");
+var logTableContainer = document.getElementById("table-container");
+var logObjectList     = parseLTSVLog(document.getElementById("log-input").value);
+
+submitButton.addEventListener("click", showLogTableOn(logTableContainer, logObjectList), false);
